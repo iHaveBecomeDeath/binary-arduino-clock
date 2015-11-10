bcb3c695e93511a2c7cf44593211929e4b7c7668
Kalle Wallin <kalle.wallin@gmail.com>
2015-11-10 15:53:07 +0100

    early setup of ticks based version

--- Binary_clock.ino
+++ Binary_clock.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+int ticks = 0;
+int minutes = 0;
+int hours = 0;
+const int hourPins = {2, 3, 4, 5};
+const int hourPins = {6, 7, 8, 9, 10};
+const int secondPin = 11;
 
+void setup() {
+   for (int i = 0; i < sizeOf(hourPins)/sizeOf(int)) - 1; i++){
+     pinMode(hourPins[i], OUTPUT);
+   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  toggleSecondPin();
+  delay(1000);
+  if (ticks < 60)
+  ticks++;
+}
 
+void toggleSecondPin(){
+  togglePin(secondPin);
+}
+
+void togglePin(int pinNumber){
+   if (digitalRead(pinNumber) == HIGH){
+     pinOff(pinNumber);
+   } else {
+     pinOn(pinNumber);
+   }
+}
+
+void pinOff(int pinNumber){
+    digitalWrite(pinNumber, LOW); 
+}
+
+void pinOn(int pinNumber){
+    digitalWrite(pinNumber, HIGH); 
 }
