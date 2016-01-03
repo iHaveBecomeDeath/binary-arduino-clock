c80dc96340d5b2f40869ab159629a973e4f09f7c
Kalle Wallin <kalle.wallin@gmail.com>
2016-01-03 23:58:55 +0100

    preliminary support for the 3231 chip and synching using time library

--- Binary_clock.ino
+++ Binary_clock.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
-
 #include "Time.h"
+#define DS3231_I2C_ADDRESS 0x68
 const int hourPins[] = {2, 3, 4, 5, 6};
 const int minutePins[] = {7, 8, 9, 10, 11, 12};
 const int secondPin = 13;
@@ -12,7 +12,10 @@ int previousMinute = 0;
 int previousHour = 0;
 
 void setup() {
- // Serial.begin(9600);
+  Wire.begin();
+  setSyncProvider(getRTCTime());
+  setSyncInterval(100);
+
   int i;
   for (i = 0; i < amountOfHourPins ; i++){
     initOutputPin(hourPins[i]);
@@ -21,7 +24,6 @@ void setup() {
     initOutputPin(minutePins[i]);
    }
   initOutputPin(secondPin);
-  setTime(1, 22, 55, 1, 1, 1999); // For testing. Should probably initialize to 0,0,0. Or get from NTP or what not.
   updateMinutes(minute());
   updateHours(hour());
 }
@@ -31,12 +33,11 @@ void loop() {
     if ((previousSecond < curSec) ||  (previousSecond > curSec && previousSecond == 59)){
       toggleSecondPin();
       previousSecond = curSec;
-      //updateMinutes(curSec); // Only for testing, minutes are slow :)
+      displayTime();
     } 
   int curMin = minute();
   if (previousMinute < curMin || (previousMinute > curMin && previousMinute == 59)){
     updateMinutes(curMin);
-    //updateHours(curMin); // Only for testing, hours are slow :)
     previousMinute = curMin;
   }  
   int curHr = hour();
@@ -48,14 +49,11 @@ void loop() {
 
 void updateMinutes(int mins){
   String bm = String(mins, BIN);
-  //Serial.println("orig bm: " + bm + ", mins: " + mins);
   if (bm.length() < amountOfMinutePins){
    for (int i = bm.length(); i < amountOfMinutePins; i++){
      bm = '0' + bm;
    } 
   }
-  
-  //Serial.println("bm: " + bm);
   
   for (int j = 0; j < bm.length(); j++){
    if (bm.charAt(j) == '1'){
@@ -109,4 +107,51 @@ void initOutputPin(int pinNumber){
      pinOn(pinNumber);
      delay(120);
      pinOff(pinNumber);
+}
+
+
+/***********/
+/* RTC stuff for rs3231 */
+/* borrowed with thanks from tronixstuff.com */
+/**********/
+
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+  return( (val/10*16) + (val%10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return( (val/16*10) + (val%16) );
+}
+
+void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set next input to start at the seconds register
+  Wire.write(decToBcd(second)); // set seconds
+  Wire.write(decToBcd(minute)); // set minutes
+  Wire.write(decToBcd(hour)); // set hours
+  Wire.write(decToBcd(dayOfWeek)); // set day of week (1=Sunday, 7=Saturday)
+  Wire.write(decToBcd(dayOfMonth)); // set date (1 to 31)
+  Wire.write(decToBcd(month)); // set month
+  Wire.write(decToBcd(year)); // set year (0 to 99)
+  Wire.endTransmission();
+}
+
+void readDS3231time(byte *second, byte *minute, byte *hour, byte *dayOfWeek, byte *dayOfMonth, byte *month, byte *year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set DS3231 register pointer to 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+  // request seven bytes of data from DS3231 starting from register 00h
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
+  *dayOfWeek = bcdToDec(Wire.read());
+  *dayOfMonth = bcdToDec(Wire.read());
+  *month = bcdToDec(Wire.read());
+  *year = bcdToDec(Wire.read());
 }
